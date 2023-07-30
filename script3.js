4e26cddfaee532e95c5ea818a746855a8b27ecbb
Chelsea Caines <chelseajcaines@hotmail.com>
2023-07-30 02:20:00 -0230

    changed things

--- script3.js
+++ script3.js
@@ -1,84 +1,65 @@
 const firstNameInput = document.getElementById("first-name")
-const firstNameError = document.querySelector('[first-name-error]');
+const firstNameError = document.querySelector("[first-name-error]")
 
 const lastNameInput = document.getElementById("last-name")
-const lastNameError = document.querySelector('[last-name-error]');
+const lastNameError = document.querySelector("[last-name-error]")
 
 const emailInput = document.getElementById("email")
-const emailError = document.querySelector('[email-error]');
+const emailError = document.querySelector("[email-error]")
 
 const phoneNumberInput = document.getElementById("phone")
-const phoneNumberError = document.querySelector('[phone-number-error]');
+const phoneNumberError = document.querySelector("[phone-number-error]")
 
 const passwordInput = document.getElementById("password")
 const confirmPasswordInput = document.getElementById("confirm")
-const passwordError = document.querySelector('[password-error]');
-
+const passwordError = document.querySelector("[password-error]")
 
 const logInButton = document.getElementById("log-in-button")
 const createButton = document.getElementById("submit")
 const message = document.getElementById("message")
 
-
 // generic callback to handle validation at the field level, takes an input element and an error display element
 const handleFieldValidation = (input_element, error_element) => {
-    if (!input_element.validity?.valid) error_element.style.display = 'block';
-    else error_element.style.display = 'none';
+    if (!input_element.validity?.valid) error_element.style.display = "block"
+    else error_element.style.display = "none"
 }
 
 // Input listeners for handling field-level validation
-firstNameInput.addEventListener('input', () => handleFieldValidation(firstNameInput, firstNameError));
-lastNameInput.addEventListener('input', () => handleFieldValidation(lastNameInput, lastNameError));
-emailInput.addEventListener('input', () => handleFieldValidation(emailInput, emailError));
-phoneNumberInput.addEventListener('input', () => handleFieldValidation(phoneNumberInput, phoneNumberError));
+// if any field is left empty or entered incorrectly, the inputs will notify the user and the form will not submit
+firstNameInput.addEventListener("input", () =>
+    handleFieldValidation(firstNameInput, firstNameError)
+)
+lastNameInput.addEventListener("input", () =>
+    handleFieldValidation(lastNameInput, lastNameError)
+)
+emailInput.addEventListener("input", () =>
+    handleFieldValidation(emailInput, emailError)
+)
+phoneNumberInput.addEventListener("input", () =>
+    handleFieldValidation(phoneNumberInput, phoneNumberError)
+)
 
-// Checks existing values in password inputs and return a boolean check
-const validatePasswords = () => {
-    if(
-        !passwordInput.value || !confirmPasswordInput.value ||
-        !passwordInput.validity.valid || !confirmPasswordInput.validity?.valid
-    ) {
-        passwordError.style.display = 'block';
-        return false
-    }
-    if(passwordInput.value === confirmPasswordInput.value) 
-    {
-        passwordError.style.display = 'none';
-        return true
-    }
-    return false
-}
+const form = document.getElementById("form")
 
-// function checkPassword() {
-//     if (passwordInput.length != 0) {
-//         if (passwordInput != confirmPasswordInput) {
-//             message.textContent = "Password does not match"
-//             message.style.color = "#f50606"
-//         } else {
-//             alert("You have created your account")
-//         }
-//     }
-// }
-
-const enterUserInfo = () => {
-    //If every field is entered correctly
+// actual form listening for submit event which happens when clicking the 'create account' button
+form.addEventListener("submit", (event) => {
+    // if either or both password fields are empty or are entered but do not fit the valid format...
+    // inputs will automatically tell the user they are not entered correctly and the form will not submit
     if (
-        firstNameInput.validity?.valid &&
-        lastNameInput.validity?.valid &&
-        emailInput.validity?.valid &&
-        phoneNumberInput.validity?.valid &&
-        validatePasswords()
+        !passwordInput.value ||
+        !confirmPasswordInput.value ||
+        !passwordInput.validity.valid ||
+        !confirmPasswordInput.validity?.valid ||
+        // if both password values are not equal to each other...
+        // the confirmPasswordInput border will turn red, an error message pops up and the form will not submit
+        passwordInput.value != confirmPasswordInput.value
     ) {
-        alert("You have created your account")
-
-        //If something is left out or not entered correctly
-    } else {
-        // Validation is already being handled here, no need for extra alerts from JS
+        passwordError.style.display = "block"
+        confirmPasswordInput.style.borderColor = "#f50606"
+        event.preventDefault()
     }
-}
-
-//clicking the 'create account' button
-createButton.addEventListener("click", () => enterUserInfo())
+    // when everything is entered correctly, the form will submit and the page will refresh
+})
 
 const goToLogin = () => {
     alert("Great to see you again!")
